6760a78e6077cb6c3762c5b40264df99fa153a55
bl0ssom <86112607+bl0ssom@users.noreply.github.com>
2021-08-04 21:29:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
-        let myLeads = []
-        const inputEl = document.getElementById("input-el")
-        const inputBtn = document.getElementById("input-btn")
-        const ulEl = document.getElementById("ul-el")
-        const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-        const tabBtn = document.getElementById("tab-btn")
+let myLeads = {}
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+const tabBtn = document.getElementById("tab-btn");
 
-        if (leadsFromLocalStorage) {
-            myLeads = leadsFromLocalStorage
-            render(myLeads)
-        }
+if (leadsFromLocalStorage != undefined) {
+    myLeads = leadsFromLocalStorage
+    for (var uid in myLeads) { render(uid); }
+}
 
+function randomGUID() {
+    let uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) { var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8); return v.toString(16); });
+    if (uid in myLeads) randomGUID();
+    else return uid;
+}
 
+function saveList() { localStorage.setItem("myLeads", JSON.stringify(myLeads)) }
 
-        tabBtn.addEventListener("click", function() {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                myLeads.push(tabs[0].url)
-                localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-                render(myLeads)
-            })
+function removeElement(element) {
+    delete myLeads[element.parentNode.id];
+    document.getElementById(element.parentNode.id).remove();
+    saveList();
+}
 
-        });
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) );
+        render(myLeads);
+    })
+});
 
+function render(uid) {
+    let text = myLeads[uid];
+    let listElement = document.createElement("li");
+    listElement.setAttribute("id", uid);
 
-        function render(leads) {
-            let listItems = ""
-            for (let i = 0; i < leads.length; i++) {
-                listItems += `
-                    <li>
-                        <a target='_blank' href='${leads[i]}'>
-                            ${leads[i]}
-                        </a>
-                    </li>
-                `
-            }
-            ulEl.innerHTML = listItems  
-        }
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("target", "_blank");
+    linkElement.setAttribute("href", text);
+    linkElement.innerText = text;
 
+    let buttonElement = document.createElement("button");
+    buttonElement.setAttribute("onclick", "removeElement(this);");
+    buttonElement.innerText = "Remove";
 
+    listElement.appendChild(linkElement);
+    listElement.appendChild(buttonElement);
+    ulEl.appendChild(listElement);
+}
 
-
-
-        inputBtn.addEventListener("click", function() {
-            myLeads.push(inputEl.value)
-            inputEl.value = ""
-            localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-            render(myLeads)
-        });
+inputBtn.addEventListener("click", function() {
+    uid = randomGUID();
+    myLeads[uid] = inputEl.value;
+    inputEl.value = "";
+    saveList();
+    render(uid);
+});
 
